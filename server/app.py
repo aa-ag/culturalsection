55319a0f0e0c5b24493e16ad744dc00cff1ac36b
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-04 10:06:18 -0500

    Fix autocomplete typo on route handler's methods

--- server/app.py
+++ server/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 ############------------ ROUTE(S) ------------############
-@app.route('/', methodcaller=['GET'])
+@app.route('/', methods=['GET'])
 def hello():
     return jsonify('World')
 
