4bbca01d4e511ef742894ff93a942dad3b0e5351
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-05 20:31:35 -0500

    Add script

--- client/src/components/World.vue
+++ client/src/components/World.vue
@@ -3,3 +3,13 @@
         <p>{{ msg }}</p>
     </div>
 </template>
+<script>
+export default {
+    name: 'World',
+    data() {
+        return {
+            msg: 'testing 123, testing. testing.'
+        };
+    },
+};
+</script>
