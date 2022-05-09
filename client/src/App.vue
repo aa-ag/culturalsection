d7b2daae89cb1ca98556d404bd6eb3bdbca14511
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-08 19:48:39 -0500

    Add navbar to app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,20 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Cultural Section</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
   <router-view />
 </template>
 
