4b6b1f7e617d294e52518e42b21d1dc3d0d8286f
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-12 20:16:47 -0500

    Import and register vue-bootstrap-typeahead

--- client/src/main.js
+++ client/src/main.js
@@ -3,5 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import 'bootstrap/dist/css/bootstrap.css';
+import VueBootstrapTypeahead from 'vue-bootstrap-typeahead';
 
 createApp(App).use(store).use(router).mount("#app");
+
+Vue.component('vue-bootstrap-typeahead', VueBootstrapTypeahead)
