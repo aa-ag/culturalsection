a3f43b24e2500ae4181734be0a0b29f0d42bf3d5
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-13 18:54:05 -0500

    Add container with datalist

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,5 +1,15 @@
 <template>
-
+  <div class="container">
+    <label for="exampleDataList" class="form-label">Datalist example</label>
+    <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search...">
+    <datalist id="datalistOptions">
+      <option value="San Francisco"></option>
+      <option value="New York"></option>
+      <option value="Seattle"></option>
+      <option value="Los Angeles"></option>
+      <option value="Chicago"></option>
+    </datalist>
+  </div>
 </template>
 
 <script>
