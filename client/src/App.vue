4b692354a44485e92cc33e967156287654ccb6e0
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-14 09:12:01 -0500

    Edit nav bar section names

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,13 +7,10 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
+          <a class="nav-link" href="#">Help</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Contact</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Legal</a>
         </li>
       </ul>
     </div>
