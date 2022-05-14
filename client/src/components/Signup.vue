d69a994e8f357e9382b23ec086120ac1c614b6af
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-14 14:17:35 -0500

    Add signup component

---
+++ client/src/components/Signup.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <form>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <div class="col-auto">
+          <span id="passwordHelpInline" class="form-text">
+            Must be 16-20 characters long.
+          </span>
+        </div>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+        <a href="reset" class="stretched-link">Forgot password?</a>
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">I agree with the Legal stuff</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+</template>
+<style>
+.container {
+  padding-top: 1.5rem;
+  height: 29rem;
+}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../components/Home.vue";
 import Login from "../components/Login.vue";
+import Signup from "../components/Signup.vue";
 import Help from "../components/Help.vue";
 import Contact from "../components/Contact.vue";
 import About from "../components/About.vue";
@@ -17,6 +18,11 @@ const routes = [
     path: "/login",
     name: "Login",
     component: Login,
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
   },
   {
     path: "/help",
