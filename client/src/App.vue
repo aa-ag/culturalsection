b78ce7b3b8f0998fd734561d5025273fbdffb1b5
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-14 11:58:18 -0500

    Link to each component as expected

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,21 @@
 <template>
+  <div id="app">
   <nav class="navbar navbar-expand-md navbar-light bg-light">
-    <a class="navbar-brand" href="#">Cultural Section</a>
+    <a class="navbar-brand" href="/">Cultural Section</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">Help</a>
+          <a class="nav-link" href="help">Help</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
+          <a class="nav-link" href="contact">Contact</a>
         </li>
       </ul>
       <ul class="navbar-nav flex-row flex-wrap ms-md-auto">
-        <a class="btn btn-dark" href="#" role="button">Diplomatic Missions</a>
+        <a class="btn btn-dark" href="login" role="button">Diplomatic Missions</a>
       </ul>
     </div>
   </nav>
@@ -25,10 +26,11 @@
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-1 my-2">
       <p class="col-md-4 mb-0 text-muted">&copy; 2022</p>
       <ul class="nav col-md-4 justify-content-end">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Legal</a></li>
+        <li class="nav-item"><a href="about" class="nav-link px-2 text-muted">About</a></li>
+        <li class="nav-item"><a href="legal" class="nav-link px-2 text-muted">Legal</a></li>
       </ul>
     </footer>
+  </div>
   </div>
 </template>
 
