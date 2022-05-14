c402e1dc0c2c47ed0e05d7c9c2ccaebbe98aa952
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-14 15:13:13 -0500

    Add form in directory for searching for embassies or consulates

--- client/src/components/Directory.vue
+++ client/src/components/Directory.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
-    <p>Directory</p>
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="Embassy or Consulate" aria-label="Embassy or Consulate">
+      <span class="input-group-text">of</span>
+      <input type="text" class="form-control" placeholder="Canada" aria-label="Canada">
+      <span class="input-group-text">in</span>
+      <input type="text" class="form-control" placeholder="the United States" aria-label="the United States">
+      <button type="submit" class="btn btn-primary">Search</button>
+    </div>
   </div>
 </template>
 <style>
