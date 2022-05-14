06aae69c22b7ac895129cc4b053879f9d4eeea13
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-14 14:19:22 -0500

    Edit buttons' copy for login and signup

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -15,11 +15,7 @@
         <input type="password" class="form-control" id="exampleInputPassword1">
         <a href="reset" class="stretched-link">Forgot password?</a>
     </div>
-    <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">I agree with the Legal stuff</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary">Login</button>
     </form>
   </div>
 </template>
--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -18,7 +18,7 @@
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
         <label class="form-check-label" for="exampleCheck1">I agree with the Legal stuff</label>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary">Signup</button>
     </form>
   </div>
 </template>
