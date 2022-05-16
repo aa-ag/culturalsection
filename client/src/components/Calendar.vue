45f0d39c8df8df3c2975e64bec6a3e8f0b815d3e
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-15 19:35:30 -0500

    Add calendar bare bones from simple-calendar's example

--- client/src/components/Calendar.vue
+++ client/src/components/Calendar.vue
@@ -1,8 +1,35 @@
 <template>
-  <div class="container">
-    <p>Calendar</p>
-  </div>
+	<div class="container">
+		<h1>My Calendar</h1>
+		<calendar-view
+			:show-date="showDate"
+			class="theme-default holiday-us-traditional holiday-us-official">
+			<template #header="{ headerProps }">
+				<calendar-view-header
+					:header-props="headerProps"
+					@input="setShowDate" />
+			</template>
+		</calendar-view>
+	</div>
 </template>
+<script>
+	import { CalendarView, CalendarViewHeader } from "vue-simple-calendar"
+	export default {
+		name: 'app',
+		data: function() {
+			return { showDate: new Date() }
+		},
+		components: {
+			CalendarView,
+			CalendarViewHeader,
+		},
+		methods: {
+			setShowDate(d) {
+				this.showDate = d;
+			},
+		}
+	}
+</script>
 <style>
 .container {
   padding-top: 1.5rem;
