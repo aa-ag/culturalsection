176b7523d1b07062e2359c5f8c6a97b0b04d6d9f
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-16 20:59:59 -0500

    Add id and country to Mission model

--- server/models.py
+++ server/models.py
@@ -2,6 +2,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+### set up
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
+
+### Mission
+class Mission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    country = db.Column(db.String(80), nullable=False)
