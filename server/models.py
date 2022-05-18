649f3354848adc7fc8ff2983f5ff93681546b63b
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-17 21:21:02 -0500

    Add url to flag as a string of 255 characters to start, might decide to store images differently all-together:wq

--- server/models.py
+++ server/models.py
@@ -12,6 +12,7 @@ class Mission(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     country = db.Column(db.String(2), nullable=False)
     city = db.Column(db.String(3), nullable=False)
+    flag_url = db.Column(db.String(255), nullable=False)
     '''
     - flag
     - team
