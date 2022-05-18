f0532e077aa0b25d17c3b382d4b9268446bfd112
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-17 21:29:50 -0500

    Provisionally rename fields to be altho longer, more explicit in their meanings

--- server/models.py
+++ server/models.py
@@ -10,12 +10,12 @@ db = SQLAlchemy(app)
 ### Mission
 class Mission(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    country = db.Column(db.String(2), nullable=False)
-    city = db.Column(db.String(3), nullable=False)
-    flag_url = db.Column(db.String(255), nullable=False)
+    home_country = db.Column(db.String(2), nullable=False)
+    home_country_flag_url = db.Column(db.String(255), nullable=False)
+    destination_city = db.Column(db.String(3), nullable=False)
     # team = this probably needs to be a different table
-    address = db.Column(db.Text(), nullable=False)
-    phone = db.Column(db.Numeric(), nullable=False)
+    destination_address = db.Column(db.Text(), nullable=False)
+    destination_phone = db.Column(db.Numeric(), nullable=False)
     '''
     - flag
     - team
