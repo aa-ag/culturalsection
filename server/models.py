ada61c46960e8c7cddb659e47af6c9c43df51ee8
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-19 20:46:37 -0500

    Add when and where for each event in Event class

--- server/models.py
+++ server/models.py
@@ -39,6 +39,9 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     mission = db.Column(db.Integer, foreing_key=True, nullable=False)
     title = db.Column(db.Text(), nullable=False)
+    description = db.Column(db.Text(), nullable=False)
+    when = db.Column(db.DateTime(), nullable=False)
+    where = db.Column(db.Text(), nullable=False)
 
 class Service(db.Model):
     id = db.Column(db.Integer, primary_key=True)
