17193357bc14d0589cdceddf7fe3aa65fe9a32df
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-22 14:27:20 -0500

    Refactor Mission model & rename models.py back to models

--- server/models.py
+++ server/models.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
-
+db = SQLAlchemy()
 
 
 ############------------ MODEL(S) ------------############
@@ -14,18 +14,23 @@ class Mission(db.Model):
      and those missions have both embassies in destination countries,
      and consulates in other cities in destination countriesß
     '''
+    __tablename__ = 'mission'
+    
     id = db.Column(db.Integer, primary_key=True)
     home_country = db.Column(db.String(2), nullable=False)
     home_country_flag_url = db.Column(db.String(255), nullable=False)
     destination_city = db.Column(db.String(3), nullable=False)
-    # team = this probably needs to be a different table
+    team = db.Column(db.JSON(), nullable=True)
     destination_address = db.Column(db.Text(), nullable=False)
     destination_phone = db.Column(db.Numeric(), nullable=False)
     team = db.Column(db.JSON(), nullable=False)
-    
-    '''
-    - social channels / links
-    '''
+    social_links = db.Column(db.JSON(), nullable=False)
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
 
 class Human(db.Model):
     id = db.Column(db.Integer, primary_key=True)
