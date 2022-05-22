44b64cbd517eb96de53136b40408717408524604
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-22 15:09:25 -0500

    Refactore database now table created

--- server/database.py
+++ server/database.py
@@ -2,14 +2,19 @@
 import psycopg2
 from settings import db_url
 
+############------------ QUERIES ------------############
+CREATE_MISSION_TABLE = '''
+CREATE TABLE IF NOT EXISTS mission (
+    home_country TEXT,
+    destination_city TEXT
+);
+'''
+
 ############------------ GLOBAL VARIABLE(S) ------------############
 connection = psycopg2.connect(db_url)
-
-cursor = connection.cursor(db_url)
-
-############------------ FUNCTION(S) ------------############
-def create_tables():
-    pass
-
-
-############------------ QUERIES ------------############
+cursor = connection.cursor()
+cursor.execute(CREATE_MISSION_TABLE)
+cursor.execute("SELECT version();")
+version = cursor.fetchone()
+print(version)
+connection.close()
