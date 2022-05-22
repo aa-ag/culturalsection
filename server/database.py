cb7f53248281b77855708fecf4bb3439d2c4b0c8
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-22 14:38:56 -0500

    Establish a connection  Please enter the commit message for your changes. Lines starting

--- server/database.py
+++ server/database.py
@@ -3,7 +3,7 @@ import psycopg2
 from settings import db_url
 
 ############------------ GLOBAL VARIABLE(S) ------------############
-
+connection = psycopg2.connect(db_url)
 
 ############------------ FUNCTION(S) ------------############
 
