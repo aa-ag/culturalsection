0434e68d181856bb72d153ebe02482a20907a47a
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-22 15:15:09 -0500

    Refactore database to execute functions in order

--- server/database.py
+++ server/database.py
@@ -3,18 +3,38 @@ import psycopg2
 from settings import db_url
 
 ############------------ QUERIES ------------############
+# query to create the mission table
 CREATE_MISSION_TABLE = '''
 CREATE TABLE IF NOT EXISTS mission (
     home_country TEXT,
     destination_city TEXT
 );
 '''
+# query to insert one mission
+INSERT_MISSION = """
+INSERT INTO mission (home_country, destination_city) VALUES ('USA', 'Zug');
+"""
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 connection = psycopg2.connect(db_url)
-cursor = connection.cursor()
-cursor.execute(CREATE_MISSION_TABLE)
-cursor.execute("SELECT version();")
-version = cursor.fetchone()
-print(version)
-connection.close()
+
+
+############------------ FUNCTION(S) ------------############
+def test_connection():
+    cursor = connection.cursor()
+    cursor.execute("SELECT version();")
+    version = cursor.fetchone()
+    print(version)
+    connection.close()
+
+def create_tables():
+    cursor = connection.cursor()
+    cursor.execute("SELECT version();")
+    cursor.execute(CREATE_MISSION_TABLE)
+    connection.close()
+
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    test_connection()
+    create_tables()
