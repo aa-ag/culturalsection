b846a8056202a1322fbdab0d307d290ff66aefcd
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-24 17:56:51 -0500

    Add margin-top to searchButton

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -3,13 +3,16 @@
     <label for="input-with-list">Pick a city</label>
     <b-form-input list="input-list" id="input-with-list"></b-form-input>
     <b-form-datalist id="input-list" :options="options"></b-form-datalist>
-    <button type="submit" class="btn btn-primary">View Calendar</button>
+    <button type="submit" class="btn btn-primary" id="searchButton">View Calendar</button>
   </div>
 </template>
 <style>
 .container {
   padding-top: 1.5rem;
   height: 29rem;
+}
+#searchButton {
+  margin-top: 0.6rem;
 }
 </style>
 <script>
