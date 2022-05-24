93558b03d82892201a394cb329e646d65268900c
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-23 21:05:56 -0500

    Add provisional axios script to pass data to add route from forms

--- client/src/components/Admin.vue
+++ client/src/components/Admin.vue
@@ -51,3 +51,32 @@
   height: 29rem;
 }
 </style>
+<script>
+import axios from "axios";
+export default{
+  data(){
+    return {
+      add: {
+       home_country: "",
+       destination_city: "",
+      },
+    };
+  },
+  methods: {
+    submit:function() {
+      const path = 'http://127.0.0.1:5000/add'
+      axios.post(path, {
+        name: this.add.home_country,
+        department: this.add.destination_city,
+        }
+      )
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err =>{
+        console.log(err);
+      });
+    },
+  }
+}
+</script>
