811ced2462b9f202cc76cc5b31f77424ad79da4b
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-25 08:42:58 -0500

    Move query db logic to calendar component for now

--- client/src/components/Calendar.vue
+++ client/src/components/Calendar.vue
@@ -1,13 +1,39 @@
 <template>
-<div class="container">
-	<h1>choosen city's calendar</h1>
-</div>
+  <div class="container">
+    <p>{{ example_cities }}</p>
+  </div>
 </template>
-<script>
-</script>
 <style>
 .container {
   padding-top: 1.5rem;
   height: 29rem;
 }
 </style>
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      example_cities: '',
+    };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/';
+      axios.get(path)
+        .then((res) => {
+          this.example_cities = res.data.example_cities;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error)
+        });
+    },
+  },
+  created() {
+    this.getMessage();
+  },
+};
+</script>
