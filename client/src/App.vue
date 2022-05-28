26add501edf460dfebb1b67f82216db0bda4e629
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-28 18:58:07 -0500

    Relink all App's hrefs

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,15 +7,13 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="#">Help</b-nav-item>
-          <b-nav-item href="#">Contact</b-nav-item>
+          <b-nav-item href="/help">Help</b-nav-item>
+          <b-nav-item href="/contact">Contact</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item  right>
-            Diplomatic Missions
-          </b-nav-item >
+          <b-nav-item href="/missions" right>Diplomatic Missions</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
