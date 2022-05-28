6ec728ee3e30b0af2941247b3a873579f9cedc3e
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-28 18:55:47 -0500

    Refactor nav bar to use vue-bootstrap instead of pure-bootstrap

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,24 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-      <a class="navbar-brand" href="/">Cultural Section</a>
-      <button class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="help">Help</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact">Contact</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav flex-row flex-wrap ml-auto">
-          <a class="btn btn-dark" href="missions" role="button">Diplomatic Missions</a>
-        </ul>
-      </div>
-    </nav>
+    <b-navbar toggleable="lg" type="navbar-dark bg-dark" variant="info">
+      <b-navbar-brand href="/">Cultural Section</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Help</b-nav-item>
+          <b-nav-item href="#">Contact</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item  right>
+            Diplomatic Missions
+          </b-nav-item >
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <div class="divider"></div>
     <router-view/>
     <div class="divider"></div>
