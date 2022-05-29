af51c1db40a7505c340cc5ff434cce40cbe61942
aa-ag <aaron.aguerrevere@gmail.com>
2022-05-29 14:18:46 -0500

    Refactor app to include models for now as it was having issues with relative imports

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from settings import db_url
 from flask_cors import CORS
 from flask_migrate import Migrate
-from models import *
+from sqlalchemy.dialects.postgresql import JSON
 
 ############------------ GLOBAL VARIABLE(S) ------------###########
 ### app configuration
@@ -25,6 +25,29 @@ migrate = Migrate(app, db)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
+############------------ MODEL(S) ------------###########
+class Mission(db.Model):
+    '''
+     main unit in platform: mission-centric. 
+     Countries send missions to other countries, 
+     and those missions have both embassies in destination countries,
+     and consulates in other cities in destination countries
+    '''
+    __tablename__ = 'mission'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    home_country = db.Column(db.Text(), nullable=False)
+    destination_city = db.Column(db.Text(), nullable=False)
+    # home_country_flag_url = db.Column(db.String(255), nullable=False)
+    # team = db.Column(db.JSON(), nullable=True)
+    # destination_address = db.Column(db.Text(), nullable=False)
+    # destination_phone = db.Column(db.Numeric(), nullable=False)
+    # team = db.Column(db.JSON(), nullable=False)
+    # social_links = db.Column(db.JSON(), nullable=False)
+
+    def __repr__(self):
+        return f"Mission from {self.home_country} to {self.destination_city}."
+
 
 ############------------ ROUTE(S) ------------############
 @app.route('/calendar', methods=['GET'])
@@ -34,7 +57,15 @@ def calendar():
 
 @app.route('/admin', methods=['GET', 'POST'])
 def add_mission():
-    pass
+    if request.method == 'POST':
+        data = request.get_json()
+        new_mission = Mission(
+            home_country=data["homecountry"],
+            destination_city=data["destinationcity"],
+        )
+        db.session.add(new_mission)
+        db.session.commit()
+        return {"message": "new mission added!"}
 
 
 ############------------ DRIVER CODE ------------############
--- server/models.py
+++ server/models.py
@@ -1,32 +1,11 @@
 ############------------ IMPORTS ------------############
-from app import db
-from sqlalchemy.dialects.postgresql import JSON
+
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 
 
 ############------------ MODEL(S) ------------############
-class Mission(db.Model):
-    '''
-     main unit in platform: mission-centric. 
-     Countries send missions to other countries, 
-     and those missions have both embassies in destination countries,
-     and consulates in other cities in destination countries
-    '''
-    __tablename__ = 'mission'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    home_country = db.Column(db.Text(), nullable=False)
-    destination_city = db.Column(db.Text(), nullable=False)
-    # home_country_flag_url = db.Column(db.String(255), nullable=False)
-    # team = db.Column(db.JSON(), nullable=True)
-    # destination_address = db.Column(db.Text(), nullable=False)
-    # destination_phone = db.Column(db.Numeric(), nullable=False)
-    # team = db.Column(db.JSON(), nullable=False)
-    # social_links = db.Column(db.JSON(), nullable=False)
 
-    def __repr__(self):
-        return f"Mission from {self.home_country} to {self.destination_city}."
 
 
 class Human(db.Model):
