cb5166e020588144a65f1e2276b96cf0fe1cbcb7
aa-ag <aaron.aguerrevere@gmail.com>
2022-06-03 20:17:07 -0500

    Add b-form text to display as user writes for now

--- client/src/components/Directory.vue
+++ client/src/components/Directory.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container">
     <p>Directory</p>
+    <b-form-input v-model="text" placeholder="Enter a country"></b-form-input>
+    <div class="mt-2">{{ text }}</div>
   </div>
 </template>
 <style>
