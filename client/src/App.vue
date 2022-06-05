71a9d9d7e74e7324321e788e35465842c3487a3a
aa-ag <aaron.aguerrevere@gmail.com>
2022-06-05 10:49:19 -0500

    Add margin-top to lower data from footer top margin, looks vertically centered

--- client/src/App.vue
+++ client/src/App.vue
@@ -20,9 +20,9 @@
     <div class="divider"></div>
     <router-view/>
     <div class="divider"></div>
-    <footer class="fixed-bottom">
+    <footer>
       <div class="mb4">
-        <div class="text-center small">&copy; 2022</div>
+        <div class="text-center small" id="copyright">&copy; 2022</div>
         <div class="text-center small" id="more">
           <a href="/legal">Legal</a>
           <small>|</small>
@@ -48,7 +48,9 @@
   border-width: 1px 0;
   box-shadow: inset 0 0.5em 1.5em rgb(0 0 0 / 10%), inset 0 0.125em 0.5em rgb(0 0 0 / 15%);
 }
-
+#copyright {
+  margin-top: 0.8rem;
+}
 #more > * {
   padding: 0.25rem;
   color: rgba(166, 166, 166, 0.871);
