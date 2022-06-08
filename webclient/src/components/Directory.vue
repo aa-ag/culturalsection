5730abe41ea8b3d286767eb69f0122ba5095bcfb
aa-ag <aaron.aguerrevere@gmail.com>
2022-06-07 22:06:32 -0500

    Add second b-form-group for destination_city

--- webclient/src/components/Directory.vue
+++ webclient/src/components/Directory.vue
@@ -13,6 +13,18 @@
           placeholder="Enter your mission's home country">
       </b-form-input>
     </b-form-group>
+    <b-form-group
+      id="form-destination_city-group"
+      label="Destination city"
+      label-for="form-destination_city-input">
+      <b-form-input
+        id="form-destination_city-input"
+        type="text"
+        v-model="getMissionForm.destination_city"
+        required
+        placeholder="Enter your mission's destination city">
+      </b-form-input>
+    </b-form-group>
     <b-button type="submit" variant="primary">Submit</b-button>
     </b-form>
     <div>{{ count }}</div>
