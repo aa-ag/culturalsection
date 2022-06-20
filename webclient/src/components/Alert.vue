6ed08448c6690ab66205caa4173978bba90c3760
aa-ag <aaron.aguerrevere@gmail.com>
2022-06-20 11:21:39 -0500

    export default response

--- webclient/src/components/Alert.vue
+++ webclient/src/components/Alert.vue
@@ -1,3 +1,12 @@
 <template>
-    <p>Working!</p>
+    <div>
+        <b-alert variant="success" show>
+            {{ response }}
+        </b-alert>
+    </div>
 </template>
+<script>
+export default {
+  props: ['response'],
+};
+</script>
