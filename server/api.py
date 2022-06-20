2216faf00c24a251443c2fb5e4b14566e3e42bab
aa-ag <aaron.aguerrevere@gmail.com>
2022-06-20 11:05:09 -0500

    Remove models and config to their own files and import them as necessary

--- server/api.py
+++ server/api.py
@@ -1,76 +1,11 @@
 ############------------ IMPORTS ------------############
-from flask import Flask, jsonify, request
+### external imports
+from flask import jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from settings import db_url
-from flask_cors import CORS
-from flask_migrate import Migrate
 from sqlalchemy.dialects.postgresql import JSON
-from pprint import pprint
-
-############------------ GLOBAL VARIABLE(S) ------------###########
-### app configuration
-DEBUG = True
-
-### app instantiation
-app = Flask(__name__)
-app.config.from_object(__name__)
-
-### db configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-### enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
-
-
-############------------ MODEL(S) ------------###########
-class Mission(db.Model):
-    '''
-     main unit in platform: mission-centric. 
-     Countries send missions to other countries, 
-     and those missions have both embassies in destination countries,
-     and consulates in other cities in destination countries
-    '''
-    __tablename__ = 'mission'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    home_country = db.Column(db.Text(), nullable=False)
-    destination_city = db.Column(db.Text(), nullable=False)
-    # home_country_flag_url = db.Column(db.String(255), nullable=False)
-    # team = db.Column(db.JSON(), nullable=True)
-    # destination_address = db.Column(db.Text(), nullable=False)
-    # destination_phone = db.Column(db.Numeric(), nullable=False)
-    # team = db.Column(db.JSON(), nullable=False)
-    # social_links = db.Column(db.JSON(), nullable=False)
-
-    def __repr__(self):
-        return f"Mission from {self.home_country} to {self.destination_city}."
-
-class Human(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    mission = db.Column(db.Integer, nullable=False)
-    title = db.Column(db.Text(), nullable=False)
-    first_name = db.Column(db.Text(), nullable=False)
-    last_name = db.Column(db.Text(), nullable=False)
-
-class Event(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    mission = db.Column(db.Integer, nullable=False)
-    title = db.Column(db.Text(), nullable=False)
-    description = db.Column(db.Text(), nullable=False)
-    when = db.Column(db.DateTime(), nullable=False)
-    where = db.Column(db.Text(), nullable=False)
-
-class Service(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    mission = db.Column(db.Integer, nullable=False)
-
-class FQA(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    mission = db.Column(db.Integer, nullable=False)
+### internal imports
+from configuration import *
+from models import *
 
 ############------------ ROUTE(S) ------------############
 @app.route('/calendar', methods=['GET'])
