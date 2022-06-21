e76d81d7a08b29fc5afd9022f18c8af4c2f67743
aa-ag <aaron.aguerrevere@gmail.com>
2022-06-21 18:57:05 -0500

    Edit href for Contact now redirecting to browse

--- webclient/src/App.vue
+++ webclient/src/App.vue
@@ -8,7 +8,7 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item href="/help">Help</b-nav-item>
-          <b-nav-item href="/contact">Contact</b-nav-item>
+          <b-nav-item href="/browse">Browse</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
